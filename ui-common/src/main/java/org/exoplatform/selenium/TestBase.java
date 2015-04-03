15e41644d619a9da0ed1e441861c09712cdd46f6
phuongdt <phuongdt@exoplatform.com>
2015-04-03 16:32:37 +0700

    phuong test

--- ui-common/src/main/java/org/exoplatform/selenium/TestBase.java
+++ ui-common/src/main/java/org/exoplatform/selenium/TestBase.java
@@ -443,7 +443,7 @@ public class TestBase {
 		profile.setPreference("dom.max_script_run_time", 0);
 		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
 		capabilities.setCapability(FirefoxDriver.PROFILE, profile);
-		capabilities.setCapability("platform", Platform.LINUX);
+		capabilities.setCapability("platform", Platform.ANY);
 		capabilities.setCapability("binary", "/home/cmugnier/Downloads/firefox/firefox"); //for linux
 		driver= new RemoteWebDriver( new URL(nodeUrl),capabilities);
 		info("youyou");
